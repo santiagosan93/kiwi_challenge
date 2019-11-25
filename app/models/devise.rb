830761b29feae796caa53237a5b8b05d45d71781
Santiago Sanchez <soysantiagosanchezo@gmail.com>
2019-11-25 11:19:05 -0600

    renaming migration to avoid reserved word problems

--- app/models/devise.rb
+++ app/models/devise.rb
@@ -1,13 +1,13 @@
 class Devise < ApplicationRecord
   validates :timestamp,       uniqueness: true
-  validates :id,              uniqueness: true
+  validates :device_id,       uniqueness: true
 
   validates :timestamp,       presence: true
-  validates :id,              presence: true
+  validates :device_id,       presence: true
   validates :type,            presence: true
   validates :status,          presence: true
-  validates :activity_change, presence: true
 
   validates :status, inclusion: { in: %w(online offline) }
   validates :type,   inclusion: { in: %w(gateway sensor) }
+  # Devise.create(timestamp:"2017-05-01T00:00:10Z", device_id:"a4a281ad", device_type:"sensor", status:"offline")
 end
--- db/migrate/20191125165733_create_devises.rb
+++ db/migrate/20191125165733_create_devises.rb
@@ -2,10 +2,11 @@ class CreateDevises < ActiveRecord::Migration[5.2]
   def change
     create_table :devises do |t|
       t.string :timestamp
-      t.string :id
-      t.string :type
+      t.string :device_id
+
+      # Rename from type -> device_type to avoid the reserved word "type" problem
+      t.string :device_type
       t.string :status
-      t.integer :activity_change
 
       t.timestamps
     end
