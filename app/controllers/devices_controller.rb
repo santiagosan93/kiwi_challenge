b39ab5104574016f2edb6b8ae0e24fca1bbe275e
Santiago Sanchez <soysantiagosanchezo@gmail.com>
2019-11-29 00:14:48 -0600

    implements cleaner solution to the challenge, sacrificing ammount of querys fired

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,39 +1,21 @@
 class DevicesController < ApplicationController
-  # Could not do the authentication using normal device
-  # So I made my own method to authenticate still using the
-  # token generator by --> gem 'simple_token_authentication'
-  # In order to make it work I have to disable normal authentication.
   skip_before_action :verify_authenticity_token, only: %i[proces_csv]
-
-  # Forces the user to log in and get the key for authenticity
-  # when uploading a csv file
   before_action :authenticate_user!, only: %i[index]
 
-  # method created for testing
-  # def new_csv; end
-
   def index
-    # I would prefere the use of if over return unless (rubocop)
-    # Also the example doesn't have an empty line after return clause,
-    # but rubocop forces the empty line.
-
-    # This method was only TWO LINES!! before I decided to optimize the querys
-    # Don't know if to go back.
     return unless params[:device]
 
     @day = device_params[:timestamp]
-    @occurrences = Device.top_occurrences(@day)
-    @sample_ids = @occurrences.map { |key, _val| key }
-    @prev_occurrances = Device.top_occurrences_with_ids((@day.to_date - 7).to_s, @sample_ids)
-    @missing_pairs = missing_pairs(@occurrences, @prev_occurrances)
-    @prev_occurrances = fill_missing_paris(@prev_occurrances, @missing_pairs)
-    @prev_occurrances = Device.sort_prev_occurrances_to_occurrences(@prev_occurrances, @occurrences)
-    @ordered_devices = Device.popular_devices(@day, @sample_ids)
-    @counted_ordered_devices = @ordered_devices.map.with_index do |(device, _sorter), index|
-      third_element = 0
-      third_element = @prev_occurrances[index][1] if @prev_occurrances[index]
-      [device, @occurrences[index][1], third_element]
-    end
+    @devices = Device.get_devices(@day)
+  end
+
+  def device_history
+    return unless params[:device]
+
+    @day = device_params[:timestamp]
+    @type = device_params[:device_type]
+    @status = device_params[:status]
+    @all_days = get_all_days([], @day, @status)
   end
 
   def proces_csv
@@ -45,49 +27,17 @@ class DevicesController < ApplicationController
     end
   end
 
-  def device_history
-    return unless params[:device]
-
-    counter = 0
-    @device_type = device_params[:device_type]
-    @status = device_params[:status]
-    @date = device_params[:timestamp]
-    return if @date = ""
-    @devices = Device.get_device_and_status(@device_type, @status, @date)
-    @splited_devices = []
-    30.times do
-      @splited_devices << group_by_day(@devices, @date.to_date + counter)
-      counter -= 1
-    end
-  end
-
   private
 
-  def group_by_day(devices, date)
-    devices = devices.select do |device|
-      device.timestamp > date && device.timestamp < (date + 1)
+  def get_all_days(all_days, day, status)
+    all_days = all_days
+    counter = 0
+    30.times do
+      @device_count = Device.get_types((day.to_date + counter).to_s, @type, status)
+      all_days << [@device_count, (day.to_date + counter).to_s]
+      counter -= 1
     end
-    return [0, date] if devices.empty?
-
-    [devices.uniq!(&:device_serial_number).count, date]
-  end
-
-  def fill_missing_paris(prev_occurrances, missing_pairs)
-    prev_occurrances = prev_occurrances
-    missing_pairs.each do |device_id, index|
-      prev_occurrances.insert(index,[device_id, 0])
-    end
-    prev_occurrances
-  end
-
-  def missing_pairs(occ, prev_o)
-    missing_pair = []
-    occ = occ.map { |device_id, _occ| device_id }
-    prev_o = prev_o.map { |device_id, _occ| device_id }
-    occ.each do |device_id|
-      missing_pair << [device_id, occ.index(device_id)] unless prev_o.include?(device_id)
-    end
-    missing_pair
+    all_days
   end
 
   def authenticate_user_key
--- app/models/device.rb
+++ app/models/device.rb
@@ -8,84 +8,42 @@ class Device < ApplicationRecord
   validates :device_type,          inclusion: { in: %w[gateway sensor] }
   validates :status,               inclusion: { in: %w[online offline] }
 
-  def self.create_records(file_instance)
-    # Since we recieve an ActionDispatch::Http::UploadedFile instance,
-    # I decided to format that instance once again into a CSV file to keep
-    # coherence with what was being sent originaly.
-
-    csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
-    csv_file = CSV.new(file_instance.read, csv_options) # <-- Makes CSV
-    csv_file.each do |row|
-      Device.create(
-        timestamp: row['timestamp'],
-        device_serial_number: row['id'],
-        device_type: row['type'],
-        status: row['status']
-      )
-    end
-  end
-
-  def self.popular_devices(day, sample_ids)
-    device_and_occurrance = Device.where(
-      "timestamp >= ? AND timestamp <= ? AND device_serial_number IN (?)",
-      "#{day} 00:00:00", "#{day} 23:59:59", sample_ids
-    ).map { |dev| dev }.uniq!(&:device_serial_number).map do |device|
-      [device, sample_ids.find_index(device.device_serial_number)]
-    end
-    device_and_occurrance.sort_by { |_device, indexx| indexx }
-  end
-
-  def self.top_occurrences(day)
-    # This method returns an ARRAY of arrays! array[0][0] => (device_serial_number)
-    #                                         array[0][1] => (popularity)
-    # I think im doing something wrong calling the
-    # .first(10) last in the method chain. I think this, because I don't know
-    # how to protect me from all the querys that are going to get fired once
-    # I iterate over the returned array.
-    Device.where(
-      "timestamp >= ? AND timestamp <= ?",
-      "#{day} 00:00:00",
-      "#{day} 23:59:59"
-    )
+  def self.get_devices(day)
+    Device.where("timestamp >= ? AND timestamp <= ?","#{day} 00:00:00","#{day} 23:59:59")
           .group(:device_serial_number)
           .count.sort_by { |_key, value| value * -1 }
           .first(10)
   end
 
-  def self.sort_prev_occurrances_to_occurrences(prev_o, occ)
-    occ = occ.map { |device_id, _occurrence| device_id }
-    prev_o = prev_o.map do |device_id, occurrence|
-      [device_id, occurrence, occ.find_index(device_id)]
-    end
-    prev_o.sort_by { |_dev, _occ, index| index }.map { |dev, occr, _index| [dev, occr] }
-  end
-
-  def self.top_occurrences_with_ids(day, sample_ids)
-    Device.where(
-      "timestamp >= ? AND timestamp <= ? AND device_serial_number in (?)",
-      "#{day} 00:00:00",
-      "#{day} 23:59:59",
-      sample_ids
-    )
-          .group(:device_serial_number)
-          .count.first(10)
-  end
-
-  def dif_percentage(ocurrence, prev_o)
-    return "No previous record" if prev_o.zero?
-    # raise
-
-    "#{((ocurrence * 100) / prev_o) - 100}%"
-  end
-
-  def self.get_device_and_status(type, status, day)
-    before30 = (day.to_date - 30).to_s
-    Device.where(
+  def self.get_types(day, type, status)
+    amount = Device.where(
       "timestamp >= ? AND timestamp <= ? AND device_type = ? AND status = ?",
-      "#{before30} 00:00:00",
+      "#{day} 00:00:00",
       "#{day} 23:59:59",
       type,
       status
-    ).order(timestamp: :desc)
+    )
+    return 0 if amount.empty?
+
+    amount.map { |device| device }.uniq!(&:device_serial_number).count
+  end
+
+  def find_percentage(day, occurrence)
+    day = (day.to_date - 7).to_s
+    prev_occurrence = Device.where(
+      "timestamp >= ? AND timestamp <= ? AND device_serial_number = ?",
+      "#{day} 00:00:00",
+      "#{day} 23:59:59",
+      device_serial_number
+    ).count
+    calculate_percentage(prev_occurrence, occurrence)
+  end
+
+  private
+
+  def calculate_percentage(prev_o, occurrence)
+    return "No previous record" if prev_o.zero?
+
+    "#{((occurrence * 100) / prev_o) - 100}%"
   end
 end
